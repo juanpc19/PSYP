0aa20041803187f59ef3e0d0d983cac4b50ef368
juanpc19 <114072117+juanpc19@users.noreply.github.com>
2023-11-07 12:11:33 +0100

    Add files via upload

---
+++ Tema 2 Prog Procesos/pruebas/prueba1.py
@@ -0,0 +1,14 @@
+from multiprocessing import *
+def hello(name):
+    print("helloo", name)
+    #name parametro entrada en p= es "elena" como simulacion input
+    
+if __name__ == "__main__":
+    p=Process(target=hello, args=("Elena",))
+    #start para comenzar proceso especificado
+    p.start()
+    #join para ejecutar primero el que añado con join y despues el de abajo (sobreescribe prioridad)
+    p.join()
+    print("fin main")
+    
+    
