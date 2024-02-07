3154934bfc5bb648728f28640b2e3b40e9cd397f
juanpc19 <juan.perez@iesnervion.es>
2024-02-07 23:21:21 +0100

    s

--- Tema3ProgDeHilos/repaso/Ej1/carrera.py
+++ Tema3ProgDeHilos/repaso/Ej1/carrera.py
@@ -1,15 +1,27 @@
 
-from threading import Barrier, Thread
+import random
+from threading import Barrier, Condition, Event, Thread, Timer
+import time
 
 
-class Carrera (Thread):
+
+class Carrera(Thread):
     
-    def __init__(self, nombre,barrera=Barrier):
-        Thread.__init__(self, name=nombre, barrera=barrera)
+    def __init__(self, nombre, barrera=Barrier, cond=Condition, cond2=Condition):
+        Thread.__init__(self, name=nombre)
+        self.barrera = barrera
+        self.cond=cond
+        self.cond2=cond2
         
         
-    def run (self):
-        pass
-    #barrera espera 10 hilos, una vez estan todos los lanza tras un delay de 3 seg, aqui calcular tiempo en llgar cada uno (deberia ponerles un delay de varios segundos)
-    
-    
+    def run(self):
+        print("Hilo", self.name, "en posicion, esperando resto de hilos")
+        self.barrera.wait()
+        
+        
+        
+        
+        
+        
+        
+      
--- Tema3ProgDeHilos/repaso/ej1/main.py
+++ Tema3ProgDeHilos/repaso/ej1/main.py
@@ -1,19 +1,32 @@
 
 
-from threading import Barrier
+import random
+from threading import Barrier, Condition
+import time
 from carrera import Carrera
 
 
 if __name__=="__main__":
     
     barrera=Barrier(10)
+    cond=Condition()
+    cond2=Condition()
+
     hilos=[]
     
     for i in range (10):
-        hilo=Carrera(str(i),barrera)
+        hilo=Carrera(str(i), barrera, cond, cond2)
         hilo.start()
+        time.sleep(random.randint(1,5))#para ralentizar ejecucion hilos y ver funcionamiento mejor
         hilos.append(hilo)
         
     for h in hilos:
         h.join()
         
+        
+#problemas con timer comun, si timer propio el hilo se inicia dos veces 1 con run en main y otra con timer .
+#hago delay con timesleep porque si hago timer propio pa lanzar hilo solo sirve para ponerse en linea salida no para salir, 
+# una vez esta en linea salida el hilo ya ha empezado y no puedo hacerle otro start con timer,
+# y la rotura de la barrera se da sola al llegar los 10 hilos por lo que no puedo pararla durante 3 segundos o romperla a los 3 segundos
+
+#USAR SOLO UN CONDITIOJN CON 2 VARIABLES (todosLinea, salida)
